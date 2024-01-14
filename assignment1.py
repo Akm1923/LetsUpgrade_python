51fda24302317111d3b8e3486991199174d8a1cf
Akm1923 <156566212+Akm1923@users.noreply.github.com>
2024-01-14 12:52:37 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+#if else implementation
+num=int(input("enter the number:"))
+if(num==0):
+    print("the entered number is 0")
+elif(num>0):
+    print("the entered number is positive")
+if(num<0):
+    print("the entered number is negative")
